0245a95d295f10a87be0c4810d5e58c00fa418dc
David J. Brenes <davidjbrenes@gmail.com>
2021-01-16 20:37:02 +0100

    Linking master key file in server

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :migration_role, :app
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
