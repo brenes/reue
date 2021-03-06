25da9cc51b4d1bd220f817997be6b8be3764b20f
David J. Brenes <davidjbrenes@gmail.com>
2021-03-06 20:14:29 +0100

    Refactoring to a model method

--- app/jobs/measure_job.rb
+++ app/jobs/measure_job.rb
@@ -3,13 +3,7 @@ class MeasureJob < ApplicationJob
 
   def perform(*args)
     if Rails.env.production?
-      bme280 = I2C::Driver::BME280.new(device: 1)
-      Measurement.create(
-        measured_at: Time.now,
-        temperature:  bme280.temperature,
-        pressure:  bme280.pressure,
-        humidity: bme280.humidity
-      )
+      Measurement.create_from_sensor
     else
       Measurement.create(
         measured_at: Time.now,
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,2 +1,12 @@
 class Measurement < ApplicationRecord
+
+  def self.create_from_sensor
+    bme280 = I2C::Driver::BME280.new(device: 1)
+    Measurement.create(
+      measured_at: Time.now,
+      temperature:  bme280.temperature,
+      pressure:  bme280.pressure,
+      humidity: bme280.humidity
+    )
+  end
 end
