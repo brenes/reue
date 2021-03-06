12830e0078e732fa3b27b7e1decba1017c2ed529
David J. Brenes <davidjbrenes@gmail.com>
2021-03-06 19:28:36 +0100

    Excluding database.yml from repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :migration_role, :app
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/master.key"
+append :linked_files, "config/master.key", "config/database.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "node_modules"
